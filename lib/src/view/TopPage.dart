898e40e1e1d02e65670d7d4e13cad4b2d3535b11
RitsukiShuto <rtk410s@gmail.com>
2023-07-26 00:33:52 +0900

    構成を変更

--- lib/src/view/TopPage.dart
+++ lib/src/view/TopPage.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:sample_webapp_on_firebase/src/components/Header.dart';
 
+import 'package:flutter/material.dart';
+import 'package:sample_webapp_on_firebase/src/components/Header.dart';
+
 class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(),
-      body: Center(
-        child: Text('Top Page'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          buildHeader(),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Text("hello");
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
